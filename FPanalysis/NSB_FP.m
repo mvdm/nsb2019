66946c8145480d669745b77792a99a114b74610b
ackrok <51799203+ackrok@users.noreply.github.com>
2019-06-30 15:44:04 -0400

    Update NSB_FP.m

--- FPanalysis/NSB_FP.m
+++ FPanalysis/NSB_FP.m
@@ -1,6 +1,6 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%      Woodshole FP Analysis Script
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  MBL NSB 2019 MOUSE Photometry Processing Script
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all; clc; close all;
 
@@ -11,39 +11,64 @@ clear all; clc; close all;
 NSB_FPparams
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%MOUSE IDENTIFYING INFORMATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+prompt = {'Enter Human ID:','Enter Mouse ID:','YYMMDD:'};
+answer = inputdlg(prompt,'Input');
+
+data = struct; %initialize data structure
+    data.humanID = answer{1}; %populate inputs
+    data.mouseID = answer{2};
+    data.recdate = answer{3};
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %LOAD DATA HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 choice = menu('data format?','.csv','.mat');
 switch choice
     case 1 
-        [dataFile,dataPath] = uigetfile('*.csv');
+        data = struct; %initialize data structure
+            data.humanID = answer{1}; %populate inputs
+            data.mouseID = answer{2};
+            data.recdate = answer{3};
         
+        cd('C:\Users\MBLUser\Desktop\NSB19_mouse\photometry')
+        [dataFile,dataPath] = uigetfile('*.csv');
+        cd(dataPath)
         opts = detectImportOptions([dataFile],'NumHeaderLines',1); % number of header lines which are to be ignored
         opts.VariableNamesLine = 2; % row number which has variable names
         opts.DataLine = 3; % row number from which the actual data starts
         
         raw = readtable([dataFile],opts); %read in .csv file into table
         
-        data = struct; data.raw = struct; %initialize variables
         tidx = [find(string(raw.Properties.VariableNames) == "Time_s_"), ...
             find(string(raw.Properties.VariableNames) == "AIn_1_Dem_AOut_1_"), ...
             find(string(raw.Properties.VariableNames) == "AIn_1_Raw"), ...
             find(string(raw.Properties.VariableNames) == "AIn_2")
             ]; %identify column indices based on variable names
         
-        data.raw.time = table2array(raw(:,tidx(1)))'; %populate data structure with vectors of signal
-        %data.raw.demod = table2array(raw(:,tidx(2)))';
-        data.raw.FP = table2array(raw(:,tidx(3)))';
-        data.raw.refSig = table2array(raw(:,tidx(4)))';
-        data.raw.Fs = 1/mean(diff(data.raw.time)); %sampling freq determined based on time stamps 
-        data.raw.nFPChan = 1;
+        data.raw = struct; %initialize variables
+            data.raw.time = table2array(raw(:,tidx(1)))'; %populate data structure with vectors of signal
+            %data.raw.demod = table2array(raw(:,tidx(2)))';
+            data.raw.FP = table2array(raw(:,tidx(3)))';
+            data.raw.refSig = table2array(raw(:,tidx(4)))';
+            data.raw.Fs = 1/mean(diff(data.raw.time)); %sampling freq determined based on time stamps 
+            data.raw.nFPChan = 1;
         
         dataFile = dataFile(1:end-4); %remove .csv from string
 
     case 2
-        [dataFile,dataPath] = uigetfile('*.mat');
-        load(fullfile(dataPath,dataFile));
+        convertH5_FP
+        analyzeFP
+        
+        %[dataFile,dataPath] = uigetfile('*.mat');
+        %load(fullfile(dataPath,dataFile));
+        
+        data.humanID = answer{1}; %populate inputs
+        data.mouseID = answer{2};
+        data.recdate = answer{3};
 end
 
 
@@ -64,7 +89,7 @@ if demodStatus == 1
         rawFP = data.raw.FP(n,:);
         refSig = data.raw.refSig(n,:);
         demodFP = digitalLIA(rawFP,refSig,Fs,lpCut,filtOrder);
-        demodFP((sigEdge*Fs)+1:end-(sigEdge*Fs)); %Removes the edge of the signal due to unwanted filtering edge effects
+        demodFP(round((sigEdge*Fs)+1):round(end-(sigEdge*Fs))); %Removes the edge of the signal due to unwanted filtering edge effects
         FP = baselineFP(demodFP,fitType,winPer);
         if dsRate ~= 0
             FP = downsample(FP,dsRate);
@@ -84,9 +109,10 @@ else
     end
 end
 
-data.final.params = params.FP;
-save([dataFile,'.mat'],'data'); %save to local folder
+data.final.params = params.FP; %save parameters used for analysis
+save([data.mouseID,'_',data.recdate,'_FP.mat'],'data'); %save to local folder
+%save([dataFile,'.mat'],'data'); %save to local folder
 cd('Z:\NSB_2019\03_MouseStriatum\data\photometry\');
-save([dataFile,'.mat'],'data'); %save to NSB server
+save([data.mouseID,'_',data.recdate,'_FP.mat'],'data'); %save to NSB server
 
 clearvars -except data 
